f9c82bf18fba6772399e54eb5b739287c3701cac
David Fishlock <davidfishlock83@gmail.com>
2024-04-20 23:21:23 +0100

    Update to api v3

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://api.openweathermap.org/'
 export const ENDPOINT_GEOCODING = 'geo/1.0/direct'
-export const ENDPOINT_ONE_CALL = 'data/2.5/onecall'
+export const ENDPOINT_ONE_CALL = 'data/3.0/onecall'
